bcb6cdaa181145e55af3d64d374e06b3f8fe39b3
Gustavo_Espinoza <gustavo.espinoza.aliste@gmail.com>
2023-05-17 11:59:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import drumPadData from "./drumPadData";
+import "./App.css";
 
-function App() {
+const DrumButton = ({ id, keyTrigger, src, handleClick }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="drum-pad" id={id} onClick={() => handleClick(id)}>
+      {keyTrigger}
+      <audio className="clip" id={`audio-${id}`} src={src} />
+    </div>
+  );
+};
+
+const App = () => {
+  const [displayText, setDisplayText] = useState("");
+
+  const handleKeyPress = (event) => {
+    const drumPad = drumPadData.find(
+      (pad) => pad.keyTrigger === event.key.toUpperCase()
+    );
+    if (drumPad) {
+      playAudio(drumPad);
+      setDisplayText(drumPad.id);
+    }
+  };
+
+  const handleClick = (id) => {
+    const drumPad = drumPadData.find((pad) => pad.id === id);
+    if (drumPad) {
+      playAudio(drumPad);
+      setDisplayText(drumPad.id);
+    }
+  };
+
+  const playAudio = (drumPad) => {
+    const audioElement = document.getElementById(`audio-${drumPad.id}`);
+    audioElement.currentTime = 0;
+    audioElement.play();
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  const renderDrumButtons = () => {
+    const drumButtonGroups = [];
+    for (let i = 0; i < drumPadData.length; i += 3) {
+      drumButtonGroups.push(
+        <div className="drum-button-group" key={i}>
+          {drumPadData.slice(i, i + 3).map((drumPad) => (
+            <DrumButton
+              key={drumPad.id}
+              id={drumPad.id}
+              keyTrigger={drumPad.keyTrigger}
+              src={drumPad.src}
+              handleClick={handleClick}
+            />
+          ))}
+        </div>
+      );
+    }
+    return drumButtonGroups;
+  };
+
+  return (
+    <div id="drum-machine">
+      <div>
+        <div id="display">{displayText}</div>
+      </div>
+      {renderDrumButtons()}
+      <footer className="footer">
+        Hecho por Gustavo Espinoza |{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/gustavo-espinoza-a4882625a/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Mi LinkedIn
+        </a>{" "}
+        | Buscando trabajo
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/drumPadData.js
@@ -0,0 +1,58 @@
+const drumPadData = [
+  {
+    id: 1,
+    keyTrigger: "Q",
+    name: "Heater 1",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    id: 2,
+    keyTrigger: "W",
+    name: "Heater 2",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    id: 3,
+    keyTrigger: "E",
+    name: "Heater 3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    id: 4,
+    keyTrigger: "A",
+    name: "Heater 4",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    id: 5,
+    keyTrigger: "S",
+    name: "Clap",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    id: 6,
+    keyTrigger: "D",
+    name: "Open-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    id: 7,
+    keyTrigger: "Z",
+    name: "Kick-n'-Hat",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    id: 8,
+    keyTrigger: "X",
+    name: "Kick",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    id: 9,
+    keyTrigger: "C",
+    name: "Closed-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+export default drumPadData;
